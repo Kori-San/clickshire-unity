bf92d5dff3358c988244b7a32cc854e689bedf4d
Tony Blard <tony.blard@epita.fr>
2023-04-26 13:37:39 +0200

    Create DialogBox

--- src/Assets/Scripts/DialogManager.cs
+++ src/Assets/Scripts/DialogManager.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class DialogManager : MonoBehaviour
 {
     private Queue<string> sentences;
+    public TextMeshProUGUI dialogText;
+    public GameObject dialogBox;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,5 +24,31 @@ public class DialogManager : MonoBehaviour
     public void StartDialog(Dialog dialog)
     {
         Debug.Log("Starting conversation with " + dialog.name);
+        sentences.Clear();
+        Debug.Log(dialog.sentences);
+        foreach (string sentence in dialog.sentences)
+        {
+            Debug.Log(sentence);
+            sentences.Enqueue(sentence);
+        }
+        dialogBox.SetActive(true);
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialog();
+            return;
+        }
+        string sentence = sentences.Dequeue();
+        dialogText.text = sentence;
+    }
+
+    public void EndDialog()
+    {
+        dialogBox.SetActive(false);
+        Debug.Log("End of dialog");
     }
 }
--- src/Assets/Scripts/DialogTrigger.cs
+++ src/Assets/Scripts/DialogTrigger.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class DialogTrigger : MonoBehaviour
 {
     public Dialog dialog;
-
+    
     public void TriggerDialog()
     {
         FindObjectOfType<DialogManager>().StartDialog(dialog);
