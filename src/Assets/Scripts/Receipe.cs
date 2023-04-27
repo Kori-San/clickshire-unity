0f7456fc870be7d2673efc3e1fba0d9c554f10f2
Tony Blard <tony.blard@epita.fr>
2023-04-27 10:39:46 +0200

    Update HDV

--- src/Assets/Scripts/Receipe.cs
+++ src/Assets/Scripts/Receipe.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+using TMPro;
 
 public class Receipe : MonoBehaviour
 {
@@ -9,11 +12,23 @@ public class Receipe : MonoBehaviour
 
     public PotionItem tempPotion;
 
+    private GameObject receipeName;
+
     // Start is called before the first frame update
     void Start()
     {
         GameObject gameObjectFinder = GameObject.Find("GameManager");
         manager = gameObjectFinder.GetComponent<GameManager>();
+        GameObject info = transform.Find("Informations")?.gameObject;
+        receipeName = info?.transform.Find("Name")?.gameObject;
+        GameObject buy = info?.transform.Find("BuyButton")?.gameObject;
+        GameObject costObject = buy?.transform.Find("CostText")?.gameObject;
+
+        
+        TextMeshProUGUI objectCostText = costObject?.GetComponentInChildren<TextMeshProUGUI>();
+        TextMeshProUGUI objectText = receipeName?.GetComponent<TextMeshProUGUI>();
+        objectText.text = tempPotion.name;
+        objectCostText.text = (tempPotion.initCost * 1.5f).ToString(manager.floatPrecision);
     }
 
     // Update is called once per frame
